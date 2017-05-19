af83cfbb84f691f5b0b376a266386fedbe23199e
Jos van Goor <josvangoor@gmail.com>
2017-05-19 20:13:14 +0200

    Renamed GUI components and started work on signals and slots.

--- QT_v1/test/mainwindow.cpp
+++ QT_v1/test/mainwindow.cpp
@@ -1,12 +1,35 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <cstdio>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    /*  
+        Link signals and slots.
+    */
+    QObject::connect(ui->sld_blinkspeed, SIGNAL(valueChanged(int)), this, SLOT(on_blinkspeed_slider_changed(int)));
 }
+
+/*
+    Personally defined slots.
+*/
+void MainWindow::on_blinkspeed_slider_changed(int val)
+{
+    std::cout << "slider changed: " << val << std::endl;
+    ui->lbl_blinkspeed->setText(QString((std::to_string(val) + "ms.").c_str()bakker
+    ));
+}
+/*
+    End of handwritten code.
+*/
 
 MainWindow::~MainWindow()
 {
--- QT_v1/test/mainwindow.h
+++ QT_v1/test/mainwindow.h
@@ -15,6 +15,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+/*
+    Personally defined slots, to receive actions from buttons etc.
+*/
+private slots:
+    void on_blinkspeed_slider_changed(int val);
+
 private:
     Ui::MainWindow *ui;
 };
